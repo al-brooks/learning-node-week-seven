ec504cfb43faaddfd6f2cc91068798acbcad5ad6
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-16 15:55:55 -0400

    beginning ui buildout

---
+++ day1/todo-list-app/client/client-app.js
@@ -0,0 +1,48 @@
+// to-do: |
+// 1. Get Todo List -  complete: |
+// 2. Post to Todo List -  complete: \
+// 3. Look at old todo list app - in-progress: |
+
+function getFullList() {
+  fetch('http://localhost:3000/todos')
+    .then((response) => response.json())
+    .then((items) => {
+      const listItems = items.map((item) => {
+        return `<li>${item.title} - ${item.priority} - ${item.dateCreated}</li>`;
+      });
+      todoList.innerHTML = listItems.join('');
+    });
+}
+
+const showList = document.querySelector('#showList');
+const todoList = document.querySelector('#todoList');
+const taskTitleInput = document.querySelector('#taskTitleInput');
+const taskPriorityInput = document.querySelector('#taskPriorityInput');
+const taskDateInput = document.querySelector('#taskDateInput');
+const addToList = document.querySelector('#addToList');
+
+showList.addEventListener('click', () => {
+  getFullList();
+});
+
+addToList.addEventListener('click', () => {
+  const title = taskTitleInput.value;
+  const priority = taskPriorityInput.value;
+  const dateCreated = taskDateInput.value;
+
+  fetch('http://localhost:3000/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title,
+      priority: priority,
+      dateCreated: dateCreated
+    })
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      getFullList();
+    });
+});
